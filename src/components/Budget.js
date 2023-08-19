d7ea0b39e8d6b588e94700bce9ec03991aa803eb
Jason Bennett <jkjbennett@gmail.com>
2023-08-18 21:37:45 -0400

    Stuck on Budget Minimum

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,36 +1,49 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import totalExpenses from "./ExpenseTotal";
 
 const Budget = () => {
-    const { budget, setBudget } = useContext(AppContext);
-    const [editableBudget, setEditableBudget] = useState(budget);
+  const { budget, setBudget } = useContext(AppContext);
+  const [editableBudget, setEditableBudget] = useState(budget);
 
-    const maxBudget = 20000;
+  const maxBudget = 20000;
 
-    const handleBudgetChange = (event) => {
-        setEditableBudget(event.target.value);
-    };
+  const handleBudgetChange = (event) => {
+    const newValue = event.target.value;
+    setEditableBudget(newValue);
 
-    const handleBudgetBlur = () => {
-        const parsedBudget = parseFloat(editableBudget);
-        if (!isNaN(parsedBudget)) {
-            setBudget(parsedBudget);
-        }
-    };
+    if (newValue > maxBudget) {
+      alert("Maximum Budget Value is " + maxBudget);
+      setEditableBudget("");
+    } else {
+      // Update global budget value only if the input is a valid number
+      const parsedBudget = parseFloat(newValue);
+      if (!isNaN(parsedBudget)) {
+        setBudget(parsedBudget);
+      }
+    }
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £</span>
-            <input
-                type='number'
-                step='10'
-                value={editableBudget}
-                onChange={handleBudgetChange}
-                onBlur={handleBudgetBlur}
-                max={maxBudget}
-            />
-        </div>
-    );
+    if (newValue < totalExpenses) {
+      alert(
+        "Budget may not be decreased under what has already been allocated."
+      );
+      setEditableBudget(totalExpenses);
+    }
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £</span>
+      <input
+        type="number"
+        step="10"
+        value={editableBudget}
+        onChange={handleBudgetChange}
+        max={maxBudget}
+        min={totalExpenses}
+      />
+    </div>
+  );
 };
 
 export default Budget;
